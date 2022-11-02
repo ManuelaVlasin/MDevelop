26fb48afdb7c2e65c2cb68e16b22a55444e44e95
raul <neculai.inocentiu@gmail.com>
2022-11-02 23:10:06 +0200

    cards

---
+++ app/Http/Livewire/Components/Cards/SimpleCardWithImage.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Livewire\Components\Cards;
+
+use App\Models\Lesson;
+use Livewire\Component;
+
+class SimpleCardWithImage extends Component
+{
+    public Lesson $lesson;
+
+    public $goToBtn = [];
+
+    public function render()
+    {
+        return view('livewire.components.cards.simple-card-with-image');
+    }
+
+    public function mount($lessonId){
+        $this->lesson = Lesson::find($lessonId);
+    }
+}
