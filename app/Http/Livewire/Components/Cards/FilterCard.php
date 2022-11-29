f3d2f4c8b71bb630f7f65cd1c91b9cf204080461
raul <neculai.inocentiu@gmail.com>
2022-11-29 03:25:14 +0200

    cards

---
+++ app/Http/Livewire/Components/Cards/FilterCard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components\Cards;
+
+use Livewire\Component;
+
+class FilterCard extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.cards.filter-card');
+    }
+}
---
+++ resources/views/livewire/components/cards/filter-card.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <div class="card" style="max-width: 200px;">
+        <div class="card-header">
+
+        </div>
+        <div class="card-body"></div>
+    </div>
+</div>
